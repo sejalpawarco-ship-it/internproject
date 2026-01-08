c4d7cc3bc8d234e09a783d6a45d6645d89e669c1
sejalpawarco-ship-it <sejalpawarco@gmail.com>
2026-01-08 16:10:32 +0530

    first setup commit

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 from typing import List
 
+
+
 def get_requirements(file_path: str) -> List[str]:
     """
     This function will return the list of requirements
